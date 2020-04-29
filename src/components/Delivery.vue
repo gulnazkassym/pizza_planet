d9884f4e691eec4b56af64555e70bb466ce318ad
Gulnaz Kassymova <gulnazkassym@gmail.com>
2020-04-29 22:38:23 +0600

    nav menu components added

---
+++ src/components/Delivery.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="info_block">
+    <h3>~ Delivery ~</h3>
+    <div class="info_block_content">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit qui quisquam, dignissimos suscipit, quam illo reprehenderit numquam, similique libero accusamus eos iusto laboriosam ullam. Illo qui reiciendis amet rerum repudiandae. </p>
+      <img src="../assets/images/delivery.png" alt="delivery image">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "delivery"
+}
+</script>
---
+++ src/components/OrderingGuide.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="info_block">
+    <h3>~ How to order ~</h3>
+    <div class="info_block_content">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit qui quisquam, dignissimos suscipit, quam illo reprehenderit numquam, similique libero accusamus eos iusto laboriosam ullam. Illo qui reiciendis amet rerum repudiandae. </p>
+      <img src="../assets/images/order.png" alt="order image">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "orderingGuide"
+}
+</script>
