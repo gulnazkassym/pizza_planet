c02758c7d38f13c36400ed89a3945fe6a3013d7a
Gulnaz Kassymova <gulnazkassym@gmail.com>
2020-04-29 22:26:30 +0600

    firebase sign in and sign out

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,11 +3,11 @@
     <form>
       <div>
         <label for="email">Email address</label>
-        <input type="email" id="email" placeholder="Enter an email"/>
+        <input type="email" id="email" placeholder="Enter an email" v-model="email"/>
       </div>
       <div>
         <label for="password">Password</label>
-        <input type="password" id="password" placeholder="Enter a password">
+        <input type="password" id="password" placeholder="Enter a password" v-model="password">
       </div>
       <button type="button" class="btn_green" @click.prevent="signIn">Sign in</button>
     </form>
@@ -19,8 +19,30 @@ import { firebaseAuth } from '../firebase'
 
 export default {
   name: 'login',
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  },
   methods: {
-    signIn() {}
+    async signIn() {
+      try {
+        await firebaseAuth.signInWithEmailAndPassword(
+          this.email,
+          this.password
+          )
+      }
+      catch(error) {
+        const errorCode = error.code
+        const errorMessage = error.message
+        if(errorCode === 'auth/wrong-password') {
+          alert('Wrong password')
+        } else {
+          alert(errorMessage)
+        }
+      }
+    }
   }
 }
 </script>
