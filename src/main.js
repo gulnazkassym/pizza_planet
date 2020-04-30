28ba7f6921ebb4926c117f1f20d8c05d8bd74ee4
Gulnaz Kassymova <gulnazkassym@gmail.com>
2020-04-30 13:23:00 +0600

    creating routes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import Home from './components/Home'
+import Menu from './components/Menu'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
+const routes = [
+  { path: '/', component: Home },
+  { path: '/menu', component: Menu }
+]
 const router = new VueRouter({
-
+  routes
 })
 
 new Vue({
