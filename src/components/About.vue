ae8568361b6f62358ecdde37a1f030d8c095b48d
Gulnaz Kassymova <gulnazkassym@gmail.com>
2020-04-30 17:34:46 +0600

    nested router view added

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,33 @@
 <template>
-  <div></div>
+  <div>
+    <h3>~ Click on the link below for more info ~</h3>
+    <nav>
+      <ul>
+        <router-link to="/history" tag="li">
+          <a class="nav-link">our history</a>
+        </router-link>
+        <span>~</span>
+        <router-link to="/delivery" tag="li">
+          <a class="nav-link">delivery</a>
+        </router-link>
+        <span>~</span>
+        <router-link to="/ordering-guide" tag="li">
+          <a class="nav-link">ordering guide</a>
+        </router-link>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'about'
+  name: "about"
 }
 </script>
+
+<style scoped>
+  h3 {
+    text-align: center;
+  }
+</style>
